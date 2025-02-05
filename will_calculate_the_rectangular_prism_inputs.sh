a70106a07d793bb2ac206e327f9ae50537294f0a
root <root@Shiva.>
2025-02-05 14:56:58 -0500

    showing error so syntax changed

--- will_calculate_the_rectangular_prism_inputs.sh
+++ will_calculate_the_rectangular_prism_inputs.sh
@@ -3,12 +3,8 @@ width =3
 height =2
 length =1
 volume=$(($length*$height*$length))
-
-
-./calculate_volume.sh $w $h $l
-
+echo $volume
 RESULT=$(./calculate_volume.sh $length $width $height)
-
 # Check if the result is greater than 0
 if [[ $RESULT -gt 0 ]]; 
 then
