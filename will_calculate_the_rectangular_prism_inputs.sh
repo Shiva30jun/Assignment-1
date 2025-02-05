80d2d65acd87d196a751818f974db30d2011af3f
root <root@Shiva.>
2025-02-05 15:23:36 -0500

    syntax changed in calculate_volume.sh

--- will_calculate_the_rectangular_prism_inputs.sh
+++ will_calculate_the_rectangular_prism_inputs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-height =2
-width =3
-length =1
+height=2
+width=3
+length=1
 
 RESULT=$(./calculate_volume.sh $height $width $length )
 # Check if the result is greater than 0
 if [[ $RESULT -gt 0 ]]; 
 then
-    echo "Success"
+    echo "Success: Volume is $RESULT"
 else 
     echo "Failed"
 fi
